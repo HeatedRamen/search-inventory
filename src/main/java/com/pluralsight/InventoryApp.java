f303438db2cf589b97e63ea92b1c6d7b62d3292f
ramenmac <kevinnguyen4200@gmail.com>
2026-04-22 14:02:21 -0500

    Exercise 3, Bonus 1, reads the product.csv, and output

--- src/main/java/com/pluralsight/InventoryApp.java
+++ src/main/java/com/pluralsight/InventoryApp.java
@@ -1,4 +1,43 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class InventoryApp {
+
+    public static void main(String[] args) throws IOException{
+
+        ArrayList<Product> inventory = getInventory();
+
+        System.out.println("We carry the following inventory: ");
+
+        //Collections.sort(inventory.name);
+
+        for (int i = 0; i < inventory.size(); i++) {
+            Product p = inventory.get(i);
+            System.out.printf("id: %d %s - Price: $%.2f%n",
+                    p.getID(), p.getName(), p.getPrice());
+        }
+    }
+    static public ArrayList<Product>getInventory() throws IOException {
+
+        ArrayList<Product> inventory = new ArrayList<>();
+        FileReader fileReader = new FileReader("inventory.csv");
+        BufferedReader bufReader = new BufferedReader(fileReader);
+
+        String newInventory = bufReader.readLine();
+        String[] parsedInventory;
+
+        while(newInventory != null){
+            parsedInventory = newInventory.split("\\|");
+            inventory.add(new Product(Integer.parseInt(parsedInventory[0]), parsedInventory[1], Double.parseDouble(parsedInventory[2])));
+
+            newInventory = bufReader.readLine();
+        }
+
+        return inventory;
+    }
+
 }
---
+++ src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Product {
+    private int ID;
+    private String name;
+    private double price;
+
+    Product(int id, String name, double price){
+        this.ID = id;
+        this.name = name;
+        this. price = price;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
